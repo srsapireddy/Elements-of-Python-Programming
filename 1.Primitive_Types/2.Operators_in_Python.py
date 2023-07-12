cb6bbde65e3d1b667f2942663afc3a5ca46f161f
sapireddyrahul@gmail.com <ssdx5@umsystem.edu>
2023-07-12 09:45:16 -0500

    Python Files

---
+++ 1.Primitive_Types/2.Operators_in_Python.py
@@ -0,0 +1,10 @@
+x = 6 
+y = 4
+print("binary value of x: ", bin(x))
+print("binary value of y: ", bin(y))
+print("AND operation: ", x&y)
+print("OR operation: ", x|y)
+print("RIGHT SHIFT operation: ", x>>1)
+print("LEFT SHIFT operation: ", x<<1)
+print("NOT operation of x: ", ~x)
+print("XOR operation: ", 2^x)
